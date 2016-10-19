6177518cf9f622e7b7fb40fd747dd2bcac40097e
Karl Hughes <khughes.me@gmail.com>
2016-10-18 20:13:36 -0500

    Getting provider to work

--- src/Providers/AdzunaProvider.php
+++ src/Providers/AdzunaProvider.php
@@ -13,70 +13,24 @@ class AdzunaProvider extends AbstractProvider
      */
     public function createJobObject($payload)
     {
-        echo json_encode($payload); exit;
-        /*
-         * Actual response:
-         * 
-            {
-              "company": {
-                "display_name": "YOOX NET A PORTER GROUP",
-                "__CLASS__": "Adzuna::API::Response::Company",
-                "canonical_name": "YOOX NET-A-PORTER GROUP"
-              },
-              "longitude": -0.233646,
-              "adref": "eyJhbGciOiJIUzI1NiJ9.eyJzIjoieVk5dFp1aXFRdFd6UXFMYXVxOTZodyIsImkiOiI0MTAyNTU4ODkifQ.9W9KUZUwtdLr018KLQ-pet3ccdeQJqKNndTyN1wJ7aA",
-              "redirect_url": "https://www.adzuna.co.uk/jobs/land/ad/410255889?se=yY9tZuiqQtWzQqLauq96hw&utm_medium=api&utm_source=e662cf9f&v=E295773E9F10D7401F66C231DC4E474A91F59174",
-              "salary_min": 42535.81,
-              "__CLASS__": "Adzuna::API::Response::Job",
-              "title": "DevOps <strong>Engineer</strong>",
-              "description": "Role purpose: We’re busy changing the way we do our technical operations and looking at the way we build our platform to use virtualisation and cloud approaches more effectively. We’re recruiting enthusiastic individuals with a passion for the DevOps approach to help us. This will involve automating everything, working closely with development teams and looking at new ways of doing things such as utilizing Cloud technologies. Working in a DevOps capacity means you’ll need some strong Linux syst…",
-              "category": {
-                "tag": "unknown",
-                "__CLASS__": "Adzuna::API::Response::Category",
-                "label": "Unknown"
-              },
-              "location": {
-                "area": [
-                  "UK",
-                  "London",
-                  "West London",
-                  "Shepherd's Bush"
-                ],
-                "__CLASS__": "Adzuna::API::Response::Location",
-                "display_name": "Shepherd's Bush, West London"
-              },
-              "id": "410255889",
-              "created": "2016-07-13T09:27:12Z",
-              "latitude": 51.508245,
-              "salary_max": 42535.81,
-              "salary_is_predicted": "1",
-              "city": null,
-              "state": null,
-              "country": null,
-              "source": null,
-              "link": null,
-              "onclick": null,
-              "guid": null,
-              "postdate": null
-            }
         $job = new Job([
             'title' => $payload['title'],
             'name' => $payload['title'],
             'description' => $payload['description'],
-            'url' => $payload['link'],
-            'sourceId' => $payload['guid'],
-            'javascriptAction' => 'onclick',
-            'javascriptFunction' => $payload['onclick'],
-            'location' => $payload['city'].', '.$payload['state'],
+            'url' => $payload['redirect_url'],
+            'sourceId' => $payload['id'],
         ]);
 
-        $job->setCompany($payload['company'])
-            ->setState($payload['state'])
-            ->setCity($payload['city'])
-            ->setDatePostedAsString($payload['postdate']);
+        $job->setDatePostedAsString($payload['created'])
+            ->setCompany($payload['company']['display_name'])
+            ->setLocation($payload['location']['display_name'])
+            ->setOccupationalCategory($payload['category']['label'])
+            ->setLongitude($payload['longitude'])
+            ->setLatitude($payload['latitude'])
+            ->setMinimumSalary($payload['salary_min'])
+            ->setMaximumSalary($payload['salary_max']);
 
         return $job;
-        */
     }
 
     /**
@@ -88,16 +42,14 @@ class AdzunaProvider extends AbstractProvider
     {
         return [
             'title',
-            'company',
-            'city',
-            'state',
-            'country',
-            'source',
-            'link',
-            'onclick',
-            'guid',
-            'postdate',
             'description',
+            'id',
+            'redirect_url',
+            'created',
+            'longitude',
+            'latitude',
+            'salary_min',
+            'salary_max',
         ];
     }
 
